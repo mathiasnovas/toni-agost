57f167c82d3defb9744bad0fc6ddec04fc4f76db
Mathias Novas <mathias.novas@try.no>
2022-07-19 12:35:25 +0200

    Espace them quotes

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -43,19 +43,19 @@ const Booking: NextPage = () => {
             and fill in the form. You will be requesting an appointment after
             submitting it.{" "}
           </p>
-          <h3 className="text-xl mt-6">What's next?</h3>
+          <h3 className="text-xl mt-6">What&apos;s next?</h3>
           <p className="mt-2">
             I evaluate your request and get back to you on the next days with
             further instructions on how to send the deposit, the exact price for
             the desired design, and the studio location. You will pay a 50€
-            deposit via PayPal or bank transfer. If you don't send the deposit
-            in the next 24 hours after my response, you will loose your spot (if
-            you transferred the deposit via bank transfer please let me know and
-            send me a transfer confirmation via email). Please let me know if
-            you decide to cancel the appointment and not pay the deposit. If you
-            booked a Custom Design the artist will draw a new design based on
-            your reference image, you can discuss with the artist if you would
-            like to add any changes during your appointment.
+            deposit via PayPal or bank transfer. If you don&apos;t send the
+            deposit in the next 24 hours after my response, you will loose your
+            spot (if you transferred the deposit via bank transfer please let me
+            know and send me a transfer confirmation via email). Please let me
+            know if you decide to cancel the appointment and not pay the
+            deposit. If you booked a Custom Design the artist will draw a new
+            design based on your reference image, you can discuss with the
+            artist if you would like to add any changes during your appointment.
           </p>
           <h3 className="text-xl mt-6">Pricing</h3>
           <p className="mt-2">
@@ -64,19 +64,18 @@ const Booking: NextPage = () => {
             your slot, no deposit no appointment. The deposit will be discounted
             from the total price. If money is an issue we can always discuss
             your budget! If you are planning to get multiple designs in one
-            session there will be discounts. Please don't book if you are not
-            ready to pay the deposit. I ONLY work with my own handwriting
+            session there will be discounts. Please don&apos;t book if you are
+            not ready to pay the deposit. I ONLY work with my own handwriting
             (cursive or capital letters) and Blackletter/Gothic , I do not do
-            other typographies. I don’t tattoo ribs (under boob is ok depending
-            the design). FLASH PREFERRED.
+            other typographies. I don&apos;t tattoo ribs (under boob is ok
+            depending the design). FLASH PREFERRED.
           </p>
           <h3 className="text-xl mt-6">Before the appointment</h3>
           <p className="mt-2">
             Make sure to eat and drink water. Bring some more water and sweet
             snacks with you ! Comfy clothes are a must ♥️ Think about the
             placement of the tattoo and wear something that makes it easy to
-            reach that area (fx: if you want it above the knee, bring shorts.
-            Please bring cash, it is also possible to pay per PayPal.
+            reach that area.
           </p>
         </Window>
       </main>
