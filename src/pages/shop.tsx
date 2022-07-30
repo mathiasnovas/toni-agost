9150ee612e3e76e2b1c52848f9db28a4bed221b9
Mathias Novas <mathias.novas@try.no>
2022-07-30 14:35:50 +0200

    Fix typo

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -6,7 +6,7 @@ const Shop: NextPage = () => {
   return (
     <div className="flex flex-col items-center md:mt-[5vh] lg:mt-[15vh] p-2">
       <Window title="Shop" actions="Let's do some shopping">
-        <div>Coming sonn...</div>
+        <div>Coming soon...</div>
       </Window>
     </div>
   );
